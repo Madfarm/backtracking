f66313dc008a4c7e0fc66c7ef694b136f6a74a04
AnthonyScott <anthony.scott1005@gmail.com>
2023-12-14 11:14:56 -0500

    Finalize permutations algorithm

--- permutations.py
+++ permutations.py
@@ -1,5 +1,25 @@
 def get_permutations(string: str):
-    return [string]
+    def helper(temp):
+        if len(temp) == len(string):
+            output.append(temp)
+            return
+        
+
+        for i in range(len(string)):
+            if (not string[i] in temp):  
+                temp += string[i]
+                helper(temp)
+                temp = temp[:len(temp)-1]
+
+    
+    output = []
+    helper("")
+
+    return output
 
 
-print(get_permutations("aaabrr"))
+    
+
+print(get_permutations("qwbr"))
+
+
